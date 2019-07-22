1d74fe5afb8ea290f63ff8f7cd172401dcbb6307
nolangendron <nolanglengendron@gmail.com>
2019-07-22 16:51:09 +0000

    Adds ArticleCards

--- client/src/components/navBar.js
+++ client/src/components/navBar.js
@@ -7,7 +7,7 @@ import InputBase from "@material-ui/core/InputBase";
 import { fade, makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 // import MenuIcon from "@material-ui/icons/Menu";
-// import SearchIcon from "@material-ui/icon/Search";
+// import SearchIcon from "@material-ui/core/IconSearch";
 
 const useStyles = makeStyles(theme => ({
   root: {
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,37 +1,64 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
+import ImgMediaCard from "../components/articleCard";
 
-const HomePage = () => (
-  <>
-    <div className="text-container">
-      <Typography>
-        <h1>Welcome to Verify It</h1>
-        <p>
-          That other text? Sadly, it’s no longer a 10. You know, it really
-          doesn’t matter what you write as long as you’ve got a young, and
-          beautiful, piece of text. My text is long and beautiful, as, it has
-          been well documented, are various other parts of my website.
-        </p>
-      </Typography>
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  }
+}));
 
-      <div className="URL-form">
-        <form>
-          <TextField
-            id="filled-full-width"
-            style={{ margin: 8 }}
-            placeholder="Enter URL..."
-            fullWidth
-            margin="normal"
-            variant="outlined"
-            InputLabelProps={{
-              shrink: true
-            }}
-          />
-        </form>
-      </div>
-    </div>
-  </>
-);
-
-export default HomePage;
+export default function HomePage() {
+  const classes = useStyles();
+  return (
+    <>
+      <Grid container spacing={3}>
+        <Grid item xs={3} />
+        <Grid item xs={6} className="text-container">
+          <Typography>
+            <h1>Welcome to Verify It</h1>
+            <p>
+              That other text? Sadly, it’s no longer a 10. You know, it really
+              doesn’t matter what you write as long as you’ve got a young, and
+              beautiful, piece of text. My text is long and beautiful, as, it
+              has been well documented, are various other parts of my website.
+            </p>
+          </Typography>
+        </Grid>
+        <Grid item xs={3} />
+        <Grid item xs={3} />
+        <Grid item xs={6}>
+          <form className="URL-form">
+            <TextField
+              id="filled-full-width"
+              style={{ margin: 8 }}
+              placeholder="Enter URL..."
+              fullWidth
+              margin="normal"
+              variant="outlined"
+              InputLabelProps={{
+                shrink: true
+              }}
+            />
+          </form>
+        </Grid>
+        <Grid item xs={3} />
+        <Grid item xs={3}>
+          <ImgMediaCard />
+        </Grid>
+        <Grid item xs={3}>
+          <ImgMediaCard />
+        </Grid>
+        <Grid item xs={3}>
+          <ImgMediaCard />
+        </Grid>
+        <Grid item xs={3}>
+          <ImgMediaCard />
+        </Grid>
+      </Grid>
+    </>
+  );
+}
