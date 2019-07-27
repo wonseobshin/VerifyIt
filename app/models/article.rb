046d0689e910d5397396cf51c29317e011bd0028
Won Seob Shin <wonseob07@gmail.com>
2019-07-27 01:23:31 +0000

    bug fix

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,9 +4,9 @@ require 'uri'
 require 'httparty'
 
 class Article < ApplicationRecord
-  # has_many :annotations
+  has_many :annotations
   # has_and_belongs_to_many :tags
-  # has_many :ratings
+  has_many :ratings
 
   after_create :getFakebox
   def getFakebox
