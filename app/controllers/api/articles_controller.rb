7af55c17f0f08277b1617a32fd7027a0a1c5f334
Won Seob Shin <wonseob07@gmail.com>
2019-07-29 18:19:27 +0000

    get docker code back

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -27,10 +27,10 @@ class Api::ArticlesController < ApplicationController
 
   def show
     article = Article.find params[:id]
-    # rating = Rating.where(:article_id => params[:id]).average(:rating)
+    rating = Rating.where(:article_id => params[:id]).average(:rating)
     tag = Tag.where(:article_id => params[:id])
-    # .average(:rating).round(1)
-    # article.getFakebox
+    .average(:rating).round(1)
+    article.getFakebox
     puts 'showing one article!'
     # puts rating
 
@@ -38,7 +38,7 @@ class Api::ArticlesController < ApplicationController
       title: article.title,
       url: article.url,
       content: article.content,
-      # rating: rating,
+      rating: rating,
       tag: tag
     }
   end
