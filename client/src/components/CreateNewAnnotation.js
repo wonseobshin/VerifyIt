66fbd596646f70536c59fdc3ccc5f3d1f03c399d
nolangendron <nolanglengendron@gmail.com>
2019-07-30 21:50:03 +0000

    Adds wider drop down menu

--- client/src/components/CreateNewAnnotation.js
+++ client/src/components/CreateNewAnnotation.js
@@ -27,7 +27,7 @@ const useStyles = makeStyles(theme => ({
   formControl: {
     margin: theme.spacing(1),
     minWidth: 120,
-    width: 240
+    width: "80%"
   },
   selectEmpty: {
     marginTop: theme.spacing(2)
